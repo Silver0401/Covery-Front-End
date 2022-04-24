a382017784b393559e2e6c9f0b64bcb4e7a56fb7
Silver0401 <prosilver05@gmail.com>
2022-04-23 21:42:29 -0500

    should fix favicon not showing up error

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,7 +11,10 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Bungee&family=Fredoka+One&family=Indie+Flower&family=Mali&family=Montserrat+Alternates&family=Permanent+Marker&family=Poiret+One&family=Righteous&display=swap"
             rel="stylesheet"
           />
-          <link rel="icon" href="/favicon.ico" />
+          <link rel="shortcut icon" href="/favicon.ico" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/favicon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon.png" />
         </Head>
         <body>
           <Main />
